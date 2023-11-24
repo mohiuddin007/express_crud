9b6590739012e70b0de452aa1109bef4bc19e56e
mohiuddin007 <mohiuddinmazumder94@gmail.com>
2023-11-24 15:43:48 +0600

    server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import app from "./app";
 import config from "./config";
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
-      console.log(`app listening on port ${config.port}`);
+      console.log(`App listening on port ${config.port}`);
     });
   } catch (err) {
     console.log(err);
